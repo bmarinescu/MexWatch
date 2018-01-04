5a3b36f1999966627199f0c3cf275c5fd257e498
edcucerzan <edcucerzan@gmail.com>
2018-01-05 00:36:15 +0200

    add bg

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="darkly.css">
+		<link rel="stylesheet" href="style.css">
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<!-- Bootstrap JavaScript -->
