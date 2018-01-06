e6249205beb196320d545431972bcba2cd7774d4
edcucerzan <edcucerzan@gmail.com>
2018-01-06 03:35:51 +0200

    icon

--- index.php
+++ index.php
@@ -47,13 +47,14 @@
 	
 ?>
 <!DOCTYPE html>
-<html lang="">
+<html lang="en-US">
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="robots" content="noindex, nofollow">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>mx</title>
+		<title>Bitmex Watcher</title>
+		<link rel="icon" type="image/png" href="icon.png" />
 		<!-- Bootstrap CSS -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
