ce95dc16fba636f91e2a2c253597625fc520ed6a
edcucerzan <edcucerzan@gmail.com>
2018-01-10 03:21:45 +0200

    new icon cache force

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
 		<meta name="robots" content="noindex, nofollow">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Bitmex Watcher</title>
-		<link rel="icon" type="image/png" href="icon.png" />
+		<link rel="icon" type="image/png" href="icon.png?v=2" />
 		<!-- Bootstrap CSS -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
