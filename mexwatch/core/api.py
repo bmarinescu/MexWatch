d225e1359993b28ec60afa076ebb0192ab2e932c
bmarinescu <bogdan.marinescu90@gmail.com>
2018-04-18 10:03:10 +0300

    adjusted incorrect dates from profit chart data

--- mexwatch/core/api.py
+++ mexwatch/core/api.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from django.utils import timezone
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import permissions
@@ -5,9 +8,10 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.utils import json
 
 from core.models import User
-from core.utils import call_bitmex_api, get_error_http_response_object_not_found, get_http_response_for_key_error, \
+from core.utils import get_error_http_response_object_not_found, call_bitmex_api, get_http_response_for_key_error, \
     get_error_http_response
 from mexwatch.settings import JSON_INDENT
+from dateutil.parser import parse
 
 charts = ["balance", "profit"]
 
@@ -18,6 +22,7 @@ charts = ["balance", "profit"]
 def get_charts(request):
     return HttpResponse(json.dumps({"charts": charts}, indent=JSON_INDENT),
                         content_type="application/json")
+
 
 @csrf_exempt
 @api_view(['GET'])
@@ -32,13 +37,12 @@ def get_chart_data(request):
     elif user_filter.count() == 0:
         return get_error_http_response_object_not_found("User", username)
     else:
-        assert False, "Multiple users with the same username in database" #should not happen
+        assert False, "Multiple users with the same username in database"  # should not happen
 
     if chart_name == "balance":
         balance_history = call_bitmex_api('/user/walletHistory', api_key=user.key_pub, api_secret=user.key_secret)
         for b in balance_history:
             b["value"] = b["walletBalance"]
-
 
         res = {"chart_type": "line",
                "data": list(reversed(balance_history))}
@@ -48,16 +52,24 @@ def get_chart_data(request):
     elif chart_name == "profit":
         balance_history = call_bitmex_api('/user/walletHistory', api_key=user.key_pub, api_secret=user.key_secret)
         profit_history = []
-        ignore_transactTypeList = ["Deposit","Withdrawal"]
+        ignore_transact_type_list = ["Deposit", "Withdrawal"]
         profit = 0
         profit_history.append({"timestamp": balance_history[-1]["timestamp"], "value": 0})
+
         for bh in reversed(balance_history):
-            if bh["transactType"] not in ignore_transactTypeList:
+            if bh["transactType"] not in ignore_transact_type_list:
                 profit += bh["amount"]
-                profit_history.append({"timestamp": bh["timestamp"], "value": profit})
+
+                now = timezone.now()
+                timestamp = parse(bh["timestamp"]) if bh["timestamp"] is not None else now
+                if timestamp > now:
+                    timestamp = now
+
+                profit_history.append({"timestamp": timestamp.strftime('%Y-%m-%dT%H:%M:%S.000Z'), "value": profit})
 
         res = {"chart_type": "line",
                "data": profit_history}
+
         return HttpResponse(json.dumps(res, indent=JSON_INDENT),
                             content_type="application/json", )
     else:
@@ -108,4 +120,3 @@ def create_user(request):
             content_type="application/json",
             status=200
         )
-
